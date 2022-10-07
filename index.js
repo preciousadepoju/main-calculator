1f8b69898e01c0468419f7a778f3a6de44cdce3b
Precious <addictcode6@gmail.com>
2022-10-07 03:31:13 +0100

    created everything together

---
+++ index.js
@@ -0,0 +1,60 @@
+function one() {
+    myInp.value += 1 
+}
+function two() {
+    myInp.value += 2 
+}
+function three() {
+    myInp.value += 3 
+}
+function four() {
+    myInp.value += 4 
+}
+function five() {
+    myInp.value += 5 
+}
+function six() {
+    myInp.value += 6 
+}
+function seven() {
+    myInp.value += 7 
+}
+function eight() {
+    myInp.value += 8 
+}
+function nine() {
+    myInp.value += 9 
+}
+function zero() {
+    myInp.value += 0 
+}
+function plus() {
+    myInp.value += '+' 
+}
+function minus() {
+    myInp.value += '-' 
+}
+function divide() {
+    myInp.value += "/" 
+}
+function equals() {
+    let result = eval(myInp.value)
+    myInp2.value = result
+    // myInp.value = ""
+}
+function point() {
+    myInp.value += '.' 
+}
+
+function cleared() {
+ myInp.value=""
+}
+function backspace() {
+    myInp.value += 7 
+}
+function percent() {
+    myInp.value += '%'
+}
+function multiply() {
+    myInp.value += '*' 
+}
